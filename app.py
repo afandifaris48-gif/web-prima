9aad649f430feaa9ecbd121a79946b1e22b2b6ad
afandifaris48-gif <afandifaris48@gmail.com>
2026-05-07 07:16:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,11 @@ def home():
 
     return render_template_string(HTML, hasil=hasil, prima=prima)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
